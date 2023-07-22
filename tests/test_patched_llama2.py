71252ab9af7b44df3a9fd04569859855d0adb11a
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-22 17:48:14 +0100

    no message

--- tests/test_patched_llama2.py
+++ tests/test_patched_llama2.py
@@ -32,13 +32,14 @@ def test_flash_attention_patch(dtype=torch.float16, device="cuda:0"):
             out1, _, _ = attn1(hidden_states, attention_mask=attn_mask, position_ids=position_ids)
             out2, _, _ = attn2(hidden_states.to(device2), attention_mask=attn_mask.to(device2), position_ids=position_ids.to(device2))
 
-            assert (((out1 - out2) * attention_mask.unsqueeze(-1)).mean(dim=-1).abs() < 1e-3).all()
+            assert (((out1 - out2.to(device)) * attention_mask.unsqueeze(-1)).mean(dim=-1).abs() < 1e-3).all()
 
         batch = tokenizer(["hello world", "lorem ipsum dolor sit amet"], padding=True, return_tensors="pt").to(device)
         out1 = model(**batch).logits
+        batch = batch.to(device2)
         out2 = patched_model(**batch).logits
 
-        diff = (out1 - out2) * batch["attention_mask"].unsqueeze(-1)
+        diff = (out1 - out2.to(device)) * batch["attention_mask"].unsqueeze(-1)
         assert (diff.abs() < 0.1).all()
 
     input_ids = torch.randint(0, model.config.vocab_size, size=(2, 10), device=device)
