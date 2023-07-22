599aff0b26ffbd7294facca3673d0b3539ff41b2
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-22 16:50:39 +0100

    no message

--- scripts/juwels_booster.sh
+++ scripts/juwels_booster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SLURM Configuration
 #SBATCH --account=cstdl
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
@@ -23,7 +23,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 # Environment Configuration
 source /p/home/jusers/clive1/juwels/clive1/miniconda3/bin/activate jordan_lora
 export WANDB_API_KEY="d8216641d549f9bb3d0c5074baa39e15dfd55030"
-export PYTHONPATH="/p/home/jusers/clive1/juwels/clive1/scaled-rope-lora:$PYTHONPATH"
+export PYTHONPATH="/p/home/jusers/clive1/juwels/clive1/scaled-rope:$PYTHONPATH"
 export TRANSFORMERS_CACHE="/p/home/jusers/clive1/juwels/clive1/transformers_cache"
 export HF_DATASETS_CACHE="/p/home/jusers/clive1/juwels/clive1/transformers_cache"
 export HF_HOME="/p/home/jusers/clive1/juwels/clive1/transformers_cache"
@@ -58,7 +58,7 @@ deepspeed --master_port 12802 \
           --hostfile '/p/home/jusers/clive1/juwels/hostfiles/hostfile.txt' \
           --master_addr $MASTER_ADDR \
           --no_ssh_check \
-          /p/home/jusers/clive1/juwels/clive1/scaled-rope-lora/finetune.py \
+          /p/home/jusers/clive1/juwels/clive1/scaled-rope/finetune.py \
           --output_dir saved_ckpts_32k \
           --configs defaults lora-7b \
           --deepspeed
